924f72f14c8184dfde89762132d4cc7a0f70d4d2
Luís André <andre2016999@gmail.com>
2024-08-18 23:44:36 -0300

    1° conceito

---
+++ 1-PilhaDeChamadas.js
@@ -0,0 +1,20 @@
+// FIFO - frist in frist out 
+// LIFO - last in frist out
+
+function firstFunction() {
+    console.log("First function starts");
+    secondFunction();
+    console.log("First function ends");
+}
+
+function secondFunction() {
+    console.log("Second function starts");
+    thirdFunction();
+    console.log("Second function ends");
+}
+
+function thirdFunction() {
+    console.log("Third function is running");
+}
+
+firstFunction();
