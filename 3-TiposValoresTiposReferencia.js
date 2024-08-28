29076e89b03890b4eff759045c4dc72fe9cc165d
Luís André <andre2016999@gmail.com>
2024-08-27 22:01:52 -0300

    Tipos de valores e tipos de referência

--- 3-TiposValoresTiposReferencia.js
+++ 3-TiposValoresTiposReferencia.js
@@ -3,13 +3,13 @@ let value1 = 10;
 let value2 = value1;
 value2 = 20;
 
-console.log(value1); // 10 (o valor original não é alterado)
-console.log(value2); // 20
+console.log(value1); 
+console.log(value2); 
 
 // Tipo de Referência
 let obj1 = { name: "Alice" };
 let obj2 = obj1;
 obj2.name = "Bob";
 
-console.log(obj1.name); // "Bob" (o valor original foi alterado)
-console.log(obj2.name); // "Bob"
+console.log(obj1.name); 
+console.log(obj2.name); 
